fdfe848766315a6e298c9f54b94cc24b5bbe208a
gulrenor <gulrenor@gmail.com>
2016-05-02 11:28:10 -0400

    Upkeep

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get update
-apt-get install -y apache2 php5-common libapache2-mod-php5 php5-cli
-rm -rf /var/www
-ln -fs /vagrant /var/www
-
-echo "webserver active on localhost:6001"
