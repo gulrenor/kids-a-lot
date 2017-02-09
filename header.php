1fc70ac8e402deb4143c7a1cbc2e76be81633d6b
gulrenor <gulrenor@gmail.com>
2017-02-09 09:19:57 -0500

    Semantic updates

--- header.php
+++ header.php
@@ -1,26 +1,25 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
+<!doctype html>
+<html lang="en">
 <head>
-    <title>Kids-a-Lot</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" type="text/css" href="css/default.css">
+  <meta charset="utf-8" />
 
+  <title>Kids-a-Lot</title>
+  <meta name="description" content="Kids-A-Lot Country Day School" />
+  <meta name="author" content="twharrison.com" />
+  
+  <link rel="stylesheet" type="text/css" href="css/default.css">
 </head>
 
 <body>
-    <header>
-        <div class="inner-header">
-            <img src="images/header-new.png" />
-            <img src="images/horse_left.gif" />
-            <img src="images/horse_right.gif" />
-        </div>
+  <header>
+    <div class="inner-header">
+      <img class="banner" src="images/header-new.png" />
+      <img class="horse" src="images/horse_left.gif" />
+      <img class="horse" src="images/horse_right.gif" />
+    </div>
 
-    </header>
+  </header>
 
-    <div class="container">
-
-        <?php include 'nav2.html'; ?>
-
-        <!--<hr class="gutter" />-->
-        <div class="content">
+  <div class="container">
+  <?php include 'nav2.html'; ?>
+    <div class="content">
